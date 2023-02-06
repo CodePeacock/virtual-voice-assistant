894e4969ef706d69b18ef5c032455fa15662a973
sharonachsah <sharonachsah13@gmail.com>
2023-02-06 21:19:26 +0530

    userinput pos

---
+++ nlptrials/pos_userinput.py
@@ -0,0 +1,19 @@
+# !pip install -U spacy
+# !python -m spacy download en_core_web_sm
+import spacy
+
+# Load English tokenizer, tagger, parser and NER
+nlp = spacy.load("en_core_web_sm")
+# Process whole documents
+text = (
+    "When Sebastian Thrun started working on self-driving cars at "
+    "Google in 2007, few people outside of the company took him "
+    "seriously. “I can tell you very senior CEOs of major American "
+    "car companies would shake my hand and turn away because I wasn’t "
+    "worth talking to,” said Thrun, in an interview with Recode earlier "
+    "this week."
+)
+doc = nlp(text)
+# Analyse syntax
+print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
+print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
